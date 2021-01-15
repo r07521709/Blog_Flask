818f67984a51209deda92a3bb70532ad7fbc015c
Hao-Yang Cheng <zhenghaoyang@Cheng.local>
2021-01-15 21:29:22 +0800

    feat: Modify routes.

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from flask_login import login_user, login_required, current_user, logout_user
 from app import app, bcrypt, db
 from app.forms import RegisterForm, LoginForm
